97efe95bd3255021ffa6d719b01fe662f7ce2fbf
Jack Liang <jack@JackdeMac-mini.local>
2023-09-03 00:03:00 +0800

    create project setting and added product list

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ProductItem from "./components/ProductItem/ProductItem.vue";
+
+interface Product {
+    id: number;
+    title: string;
+    price: number;
+    inStock: boolean;
+    imageUrl: string;
+}
+
+const products = ref<Product[]>([
+    {
+        id: 1,
+        title: "纯棉 T 恤",
+        price: 66,
+        inStock: true,
+        imageUrl: "/images/t-shirt1.jpg",
+    },
+    {
+        id: 2,
+        title: "夹克外套",
+        price: 128,
+        inStock: true,
+        imageUrl: "/images/jacket1.jpg",
+    },
+    {
+        id: 3,
+        title: "水洗牛仔裤",
+        price: 99,
+        inStock: false,
+        imageUrl: "/images/jeans1.jpg",
+    },
+    {
+        id: 4,
+        title: "印花 T恤",
+        price: 72,
+        inStock: true,
+        imageUrl: "/images/t-shirt2.jpg",
+    },
+]);
+</script>
+
+<template>
+    <main>
+        <h1>Jack's store</h1>
+        <div class="productList">
+            <ProductItem
+                v-for="product in products"
+                :key="product.id"
+                v-bind="product"
+            >
+            </ProductItem>
+        </div>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: grid;
+    place-items: center;
+    gap: 48px;
+    width: 100vw;
+    padding: 24px;
+}
+
+h1 {
+    display: flex;
+    align-items: center;
+    gap: 32px;
+}
+
+h1 svg {
+    width: 64px;
+    height: 64px;
+}
+.productList {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 48px;
+    width: 60%;
+}
+</style>
---
+++ src/components/ActionAndFilters.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="actions">
+        <button>按價格排序</button>
+        <button>按名稱排序</button>
+        <select>
+            <option value="">按庫存過濾</option>
+            <option value="true">有貨</option>
+            <option value="false">無貨</option>
+        </select>
+    </div>
+</template>
+<style scoped>
+.actions {
+    display: flex;
+    gap: 24px;
+}
+</style>
